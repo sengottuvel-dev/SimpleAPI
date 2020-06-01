d9071853ef0e1dcfbb0977aac3b3d1b802c6919b
sengottuvel-dev <sengottuvel.ssnce@yahoo.co.in>
2020-06-01 09:50:12 +0530

    updated control file

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Sengottuvel";
+            return "TesTSengottuvel";
         }
 
         // POST api/values
